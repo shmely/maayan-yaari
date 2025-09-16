9186eabfb2f4c7286064e4d02bca723ef9682572
Alon Yaari <alonya@payoneer.com>
2025-09-16 13:46:02 +0300

    more fixes

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,23 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  html: {
+    title: 'מעיין יערי',
+    tags: [
+      { tag: 'link', attrs: { rel: "icon", href: "/assets/images/logo-profile.jpg" } }
+    ],
+    meta: [
+      { name: "description", content: "מעיין יערי - מעצבת פנים מקצועית. עיצוב פנים יוקרתי ומודרני לבתים פרטיים ודירות" },
+      { name: "keywords", content: "עיצוב פנים, מעצבת פנים, עיצוב בתים, עיצוב דירות, מעיין יערי" },
+      { name: "author", content: "מעיין יערי" },
+      { property: "og:title", content: "מעיין יערי - אדריכלות ועיצוב פנים" },
+      { property: "og:description", content: "עיצוב פנים יוקרתי ומודרני לבתים פרטיים ודירות" },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://maayanyaari.co.ilm" },
+      { property: "og:image", content: "/assets/images/og-image.jpg" },
+    ],
+
+  },
+
+
 });
